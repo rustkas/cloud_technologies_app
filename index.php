986024e154832e5420f56e9bdb77972e9108c12a
Kosorukov Anatoly <java1cprog@yandex.ru>
2018-11-28 19:31:55 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<link rel="shortcut icon" type="image/png" href="/img/favicon.png" sizes="60x60"/>
+	</head>
+	<body>
+		<?php ## Проверочный скрипт Hello World.
+		echo "Hello, world";
+		?>
+	</body>
+</html>
